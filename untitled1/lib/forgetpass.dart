39127062efb07efee4d9bd9c99c8153d37ff248e
hank <C109154227@nkust.edu.tw>
2023-01-14 23:51:08 +0800

    2023/1/14 11:51

--- untitled1/lib/forgetpass.dart
+++ untitled1/lib/forgetpass.dart
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 context,
                 MaterialPageRoute(builder: (context) => MyHomePage()),
               );
-            },
+              },
           ),
           title: const Text(
             "Forgot Password",
@@ -69,110 +69,113 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Center(
           child: Container(
               child: Column(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 12,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width - 20,
-                alignment: Alignment.center,
-                child: Row(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width / 3 - 20,
-                      child: Image.asset(
-                        'lib/image/logo.jpg',
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 2 / 3 - 20,
-                      child: const Text(
-                        "請輸入您的電子信箱，我們將向您發送密碼至您的帳戶",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          color: Colors.black54,
-                          height: 1.5,
-                          fontSize: 20,
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 12,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 20,
+                    alignment: Alignment.center,
+                    child: Row(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width / 3 - 20,
+                          child: Image.asset(
+                            'lib/image/logo.jpg',
+                          ),
                         ),
-                      ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 2 / 3 - 20,
+                          child: const Text(
+                            "請輸入您的電子信箱，我們將向您發送密碼至您的帳戶",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Colors.black54,
+                              height: 1.5,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 20,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width - 20,
-                height: MediaQuery.of(context).size.height/8,
-                child: TextField(
-                    controller: pust_string_controller,
-                    cursorColor: Colors.blue,
-                    cursorRadius: const Radius.circular(4.0),
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 25.0,
-                    ),
-                    textAlign: TextAlign.left,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                        hintText: 'Email',
-                        isDense: true,
-                        filled: true,
-                        fillColor: Colors.white,
-                        suffixIcon: pust_string_controller.text != '' ? const Icon(Icons.done,) : const Icon(Icons.clear),
-                        //counterText: '',
-                        errorText: pust_string_controller.text == '' ? 'Email不得為空' : null,  //判斷是否有錯誤訊息
-                        errorStyle: TextStyle(fontSize: 15),
-                        //錯誤代碼要判斷
-                        border: OutlineInputBorder(
-                            gapPadding: 0,
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            borderSide: BorderSide(
-                              width: 1,
-                              color: Colors.white,
-                              style: BorderStyle.none,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 20,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 20,
+                    height: MediaQuery.of(context).size.height/8,
+                    child: TextField(
+                        controller: pust_string_controller,
+                        cursorColor: Colors.blue,
+                        cursorRadius: const Radius.circular(4.0),
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 25.0,
+                        ),
+                        textAlign: TextAlign.left,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            hintText: 'Email',
+                            isDense: true,
+                            filled: true,
+                            fillColor: Colors.white,
+                            suffixIcon: pust_string_controller.text != '' ? const Icon(Icons.done,) : const Icon(Icons.clear),
+                            //counterText: '',
+                            errorText: pust_string_controller.text == '' ? 'Email不得為空' : null,  //判斷是否有錯誤訊息
+                             errorStyle: TextStyle(fontSize: 15),
+                            //錯誤代碼要判斷
+                            border: const OutlineInputBorder(
+                                gapPadding: 0,
+                                borderRadius: BorderRadius.all(Radius.circular(20)),
+                                borderSide: BorderSide(
+                                  width: 1,
+                                  color: Colors.white,
+                                  style: BorderStyle.none,
+                                )
                             )
                         )
-                    )
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  String? encodeQueryParameters(Map<String, String> params) {
-                    return params.entries
-                        .map((MapEntry<String, String> e) =>
-                            '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
-                        .join('&');
-                  }
-
-                  final Uri emailLaunchUri = Uri(
-                    scheme: 'mailto',
-                    path: 'hank0910804@gmail.com',
-                    query: encodeQueryParameters(<String, String>{
-                      'subject': '寄送信件測試',
-                      'body': '您好, 寄送信件測試。'
-                    }),
-                  );
-                  launchUrl(emailLaunchUri);
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 50,
-                  decoration: BoxDecoration(
-                      borderRadius:
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      String? encodeQueryParameters(Map<String, String> params) {
+                        return params.entries
+                            .map((MapEntry<String, String> e) =>
+                        '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+                            .join('&');
+                      }
+                      final Uri emailLaunchUri = Uri(
+                        scheme: 'mailto',
+                        path: 'hank0910804@gmail.com',
+                        query: encodeQueryParameters(<String, String>{
+                          'subject': '寄送信件測試',
+                          'body': '您好, 寄送信件測試。'
+                        }
+                        ),
+                      );
+                      launchUrl(emailLaunchUri);
+                      },
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius:
                           const BorderRadius.all(Radius.circular(20.0)),
-                      color: Colors.blue,
-                      border: Border.all(color: Colors.blue)),
-                  child: const Text('寄送',
-                      style: TextStyle(fontSize: 30, color: Colors.black)),
-                ),
-              ),
-            ],
-          )),
-        ));
+                          color: Colors.blue,
+                          border: Border.all(color: Colors.blue)
+                      ),
+                      child: const Text('寄送',
+                          style: TextStyle(fontSize: 30, color: Colors.black)),
+                    ),
+                  ),
+                ],
+              )
+          ),
+        )
+    );
   }
 }
